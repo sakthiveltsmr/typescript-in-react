3a0204c423c6392a9ff026c17fd5f4d7ce64a188
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-05-04 12:58:07 +0530

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import type { FormEvent } from "react";
 import "./App.css";
+
+const sendForm = async (event: FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
+  const { name, password } = event.target as typeof event.target & {
+    name: { value: string };
+    password: { value: string };
+  };
+  console.log("name :", name.value, "password :", password.value);
+};
+//   await fetch("/route", {
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     method: "POST",
+//     body: JSON.stringify({
+//       name: name.value,
+//       password: password.value,
+//     }),
+//   });
+// };
 
 function App() {
   return (
-    <div className="App">
-      <h1>Wellcome to Typescript React</h1>
-    </div>
+    <form
+      className="wrap "
+      onSubmit={(evt) => {
+        sendForm(evt);
+      }}
+    >
+      <fieldset className="field-area">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" />
+      </fieldset>
+      <fieldset className="field-area">
+        <label htmlFor="password">Pass:</label>
+        <input type="password" id="password" />
+      </fieldset>
+      <button type="submit">login</button>
+    </form>
   );
 }
 
